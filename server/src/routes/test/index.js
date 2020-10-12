e2d2cfaa72450595dd79a91cadb05939857d8a40
jaredcscott <jaredcscott93@gmail.com>
2020-10-12 13:59:32 -0600

    adding prototype

--- server/src/routes/test/index.js
+++ server/src/routes/test/index.js
@@ -2,6 +2,6 @@ import { Router } from 'express'
 
 const router = new Router()
 
-router.get('/', (request, response) => response.json({ hello: 'world' }))
+router.get('/profile/', (request, response) => response.json({ name: 'Jared Scott', email: 'jared@msn.com', accountBal: 85 }))
 
 export default router
