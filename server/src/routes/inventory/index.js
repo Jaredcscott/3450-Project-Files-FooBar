ff7465a154afcc7513361d6f9cbd44018712df7a
OmegaGiven <9ncjohnson@gmail.com>
2020-10-14 11:39:07 -0600

    did intial changes

--- server/src/routes/inventory/index.js
+++ server/src/routes/inventory/index.js
@@ -1,22 +1,40 @@
 import { Router } from 'express'
 import { Inventory } from '../../models/inventory'
+import {default_inventory} from './inventory_data'
+import mongoose from 'mongoose'
 
 const router = new Router()
 
 // this is to populate the database with the table of data.
-router.post("/insertdefault", function(req, res){
-  Inventory.insertMany(
-    default_inventory,
-    function(err, result){
-      if (err) {
-        res.send(err);
-      } else {
-        res.send(result);
-      }
-    }
-  );
+// router.post("/insertdefault", (req, res) =>{
+//   for (var i = 0; i < default_inventory.length; i++){
+//     Inventory.insert(default_inventory[i])
+//   }
+// })
+inventory = db.collection(Inventory)
 
-});
 
+
+router.get('/', (request, response) =>
+response.json({ Inventory }))
+
+
+router.post('/register', (req, res) => {
+	Inventory.register(
+		new Inventory({
+        category: "BAGEL",
+        name: "Onion",
+        qty: 0,
+        price: 2,
+		}),
+		(err) => {
+			if (err) {
+				console.error(err)
+				res.redirect('/err')
+				return
+			}
+		}
+	)
+})
 
 export default router
