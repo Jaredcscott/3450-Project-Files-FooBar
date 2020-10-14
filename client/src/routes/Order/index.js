c7f15e1a25df5e1d7e01d06137c13806044ee8da
Peter Allen <peterjeremy@cableone.net>
2020-10-14 13:53:05 -0600

    final changes before milestone 2

--- client/src/routes/Order/index.js
+++ client/src/routes/Order/index.js
@@ -8,10 +8,10 @@ import styled from 'styled-components'
 
 export default function Order() {
 	function getBagels() {
-		return  {plain: 2, onion: 2, cinnamon_raisen: 2, sesame: 2, cheesy: 2, pumpernicel: 2}
+		return  {plain: 2, onion: 2, cinnamon_raisen: 2, sesame: 2, cheesy: 2, pumpernickel: 2}
 	}
 	function getSmears() {
-		return {plain : 1, honey_nut: 1, strawberry: 1, frech_onion: 1}
+		return {plain : 1, honey_nut: 1, strawberry: 1, french_onion: 1}
 	}
 	function getSammicheStuff () {
 		return {bacon: 1, egg: 2, cheese: 1, sausage: 2, avacado: 10, turkey: 2, ham: 2, spinach: 1, tomato: 1, lox: 10}
@@ -25,31 +25,39 @@ export default function Order() {
 	const sammicheStuff = getSammicheStuff(); 
 	const beverages = getBeverages();
 	function addSmear() {
-		let smearMenu = document.createElement("select");
+		let smearMenu = document.createElement("form");
+		smearMenu.setAttribute("id", "smear");
+		smearMenu.setAttribute("style","text-align: left")
 		for (var x in smears){
-			smearMenu.innerHTML = smearMenu.innerHTML + "<option value=\"" + x + "\">" + x + "($" + smears[x] + ") </option>"
+			smearMenu.innerHTML = smearMenu.innerHTML  + "<input type=\"checkbox\" id=\"" + x + "\" name=\"" + x + "\" value=\"" + x + "\">" + "<label for=\"" + x + "\">" + x +"($" + smears[x] + ")" +"</label>"
 		}
-		this.parentNode.insertBefore(smearMenu, this)
+		if(this.parentNode.querySelector("form") == null){
+			this.parentNode.appendChild(smearMenu)
+		}else(this.parentNode.insertBefore(smearMenu, this.parentNode.querySelector("form")))
+		
 		this.onclick = removeSmear;
 		this.textContent = "remove smear from this bagel"
 
 	}
 	function removeSmear() {
-		this.parentNode.removeChild(this.previousSibling)
+		this.parentNode.removeChild(this.parentNode.querySelector("#smear"));
 		this.onclick = addSmear;
 		this.textContent = "add a smear to this bagel"
 	}
 	function addStuff() {
-		this.textContent = "cancel the sammiche"
-		let stuffMenu = document.createElement("form")
+		this.textContent = "cancel the sammiche";
+		let stuffMenu = document.createElement("form");
+		stuffMenu.setAttribute("id", "stuff");
+		stuffMenu.setAttribute("style", "text-align: left")
+		stuffMenu.innerHTML = "<br>"
 		for (var x in sammicheStuff){
-			stuffMenu.innerHTML = stuffMenu.innerHTML + "<input type=\"checkbox\" id=\"" + x + "\" name=\"" +x + "\" value=\"" + x + "\"><label for=\"" + x + "\">" + x +"($" + sammicheStuff[x] + ")" +"</label>"
+			stuffMenu.innerHTML = stuffMenu.innerHTML + "<label for=\"" + x + "\" style=\"display: inline-block;width: 115px\"> " + x +"($" + sammicheStuff[x] + ")\t" +"</label><input type=\"numeric\" id=\"" + x + "\" name=\"" +x + "\" value=\"" + 0 + "\" style= \"display: inline-block; width: 8px\">" + "<br>"
 		}
 		this.parentNode.appendChild(stuffMenu);
 		this.onclick = removeStuff;
 	}
 	function removeStuff() {
-		this.parentNode.removeChild(this.nextSibling.nextSibling);
+		this.parentNode.removeChild(this.parentNode.querySelector("#stuff"));
 		this.textContent = "make this a sammiche";
 		this.onclick = addStuff;
 	}
@@ -102,13 +110,18 @@ export default function Order() {
 		list.appendChild(document.createElement("br"));
 	};
 
+	function sendOrder(){
+		console.log("not implemented yet");
+		}
+	}
+
 	return (
 		<Screen>
 			
 			<button style= {{width: "50%"}} onClick= {addBagle}> add bagel to order</button> 
 			<button style= {{width: "50%"}} onClick= {addBeverage}>add beverage to order</button>
 			
-			<ul id="bagleOrder" style={{textAlign: "center"}}></ul>
+			<ul id="bagelOrder" style={{textAlign: "center"}}></ul>
 			<ul id="beverageOrder"style={{textAlign: "center"}}></ul>
 			<p>
 				I would like my order to be ready at  
@@ -117,7 +130,7 @@ export default function Order() {
 				<input type="date" id="date"></input>
 			</p>
 			
-			<button style= {{width: "100%"}}>place order</button>
+			<button style= {{width: "100%"}} onClick={sendOrder} >place order</button>
 		</Screen>
 	)
 }
