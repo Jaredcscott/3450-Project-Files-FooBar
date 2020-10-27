4ac3ef097858c323bac7eec94ea5586d4e0cec8f
OmegaGiven <9ncjohnson@gmail.com>
2020-10-27 16:36:01 -0600

    cleared up analysis to try and build from scratch

---
+++ server/src/routes/analytics/index.js
@@ -0,0 +1,49 @@
+// @flow
+import { Router } from 'express'
+import { verifyUserHasRole } from '../../middleware/auth'
+import type {
+	AuthenticatedUserRequest,
+	MaybeUserRequest,
+	InventoryItemCategory,
+} from '../../utils/types'
+import { Item } from '../../models/inventoryItem'
+import { Order } from '../../models/order'
+import calculatePrice from '../order/index'
+
+import * as validate from '../../utils/validators'
+import { ROLES, INVENTORY_ITEM_CATEGORIES_ENUM } from '../../utils/constants'
+import { Types as mongooseTypes } from 'mongoose'
+
+import type {
+	AuthenticatedUserRequest,
+	Order as OrderType,
+	PopulatedOrder,
+} from '../../utils/types'
+import { ORDER_STATUS, ORDER_STATUS_ENUM, ROLES } from '../../utils/constants'
+
+
+const router = new Router()
+
+
+// return all data of an item for admin/manager
+router.get(
+	'/:id',
+	verifyUserHasRole(([ROLES.ADMIN, ROLES.MANAGER]: any)),
+	async (
+		req: AuthenticatedUserRequest<{}, {| id: string |}>,
+		res: express$Response
+	) => {
+		if (!mongooseTypes.ObjectId.isValid(req.params.id)) {
+			return res.status(400).end()
+		}
+
+
+		const analysis = {}
+		
+
+
+		return res.status(200).json({ data: analysis })
+	}
+)
+
+export default router
