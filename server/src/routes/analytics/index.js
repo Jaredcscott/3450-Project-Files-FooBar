991c960bc629597a5f005c895cfd7091893b2908
Braysen Goodwin <braysen@infinidlearning.com>
2020-10-28 15:41:08 -0600

    add analytics test

--- server/src/routes/analytics/index.js
+++ server/src/routes/analytics/index.js
@@ -8,8 +8,14 @@ import type {
 } from '../../utils/types'
 import { Item } from '../../models/inventoryItem'
 import { Order } from '../../models/order'
+import * as validate from '../../utils/validators'
 
 import { ROLES } from '../../utils/constants'
+
+const QUERY_PARAMETER_VALIDATOR = validate.isObjectWith({
+	startDate: validate.or(validate.isNumeric, validate.isUndefined),
+	endDate: validate.or(validate.isNumeric, validate.isUndefined),
+})
 
 const router = new Router()
 
@@ -18,14 +24,30 @@ router.get(
 	'/',
 	verifyUserHasRole(([ROLES.ADMIN, ROLES.MANAGER]: any)),
 	async (
-		req: AuthenticatedUserRequest<{}, {| id: string |}>,
+		req: AuthenticatedUserRequest<
+			{},
+			{},
+			{| startDate: number, endDate: number |}
+		>,
 		res: express$Response
 	) => {
+		if (!QUERY_PARAMETER_VALIDATOR(req.query)) {
+			return res.status(400).json({ reason: 'malformed request' })
+		}
+
 		const items: {
 			...InventoryItem,
-			usedInOrders?: ?number,
+			timesUsed?: ?number,
 		}[] = await Item.find().lean()
-		const orders: OrderType[] = await Order.find().lean()
+
+		const { startDate, endDate } = req.query
+		let queryParameters = {}
+
+		if (startDate && endDate) {
+			queryParameters = { placed: { $gte: startDate, $lte: endDate } }
+		}
+
+		const orders: OrderType[] = await Order.find(queryParameters).lean()
 		const itemFrequency: { [itemId: string]: number } = {}
 		const addToItemFrequency = (id: string) => {
 			if (!itemFrequency[id]) {
@@ -44,7 +66,7 @@ router.get(
 		})
 
 		items.forEach((item) => {
-			item.usedInOrders = itemFrequency[item._id] || 0
+			item.timesUsed = itemFrequency[item._id] || 0
 		})
 
 		return res.status(200).json({ data: { items, orders, totalPrice } })
