6ec69fb36d975167ce2aea29036aaa618b036904
Braysen Goodwin <braysen@infinidlearning.com>
2020-10-28 14:17:27 -0600

    ffix analytics route

--- server/src/routes/analytics/index.js
+++ server/src/routes/analytics/index.js
@@ -3,49 +3,51 @@ import { Router } from 'express'
 import { verifyUserHasRole } from '../../middleware/auth'
 import type {
 	AuthenticatedUserRequest,
-	MaybeUserRequest,
-	InventoryItemCategory,
+	Order as OrderType,
+	InventoryItem,
 } from '../../utils/types'
 import { Item } from '../../models/inventoryItem'
 import { Order } from '../../models/order'
-import calculatePrice from '../order/index'
 
-import * as validate from '../../utils/validators'
-import { ROLES, INVENTORY_ITEM_CATEGORIES_ENUM } from '../../utils/constants'
-import { Types as mongooseTypes } from 'mongoose'
-
-import type {
-	AuthenticatedUserRequest,
-	Order as OrderType,
-	PopulatedOrder,
-} from '../../utils/types'
-import { ORDER_STATUS, ORDER_STATUS_ENUM, ROLES } from '../../utils/constants'
-
+import { ROLES } from '../../utils/constants'
 
 const router = new Router()
 
-
 // return all data of an item for admin/manager
 router.get(
-	'/:id',
+	'/',
 	verifyUserHasRole(([ROLES.ADMIN, ROLES.MANAGER]: any)),
 	async (
 		req: AuthenticatedUserRequest<{}, {| id: string |}>,
 		res: express$Response
 	) => {
-		if (!mongooseTypes.ObjectId.isValid(req.params.id)) {
-			return res.status(400).end()
+		const items: {
+			...InventoryItem,
+			usedInOrders?: ?number,
+		}[] = await Item.find().lean()
+		const orders: OrderType[] = await Order.find().lean()
+		const itemFrequency: { [itemId: string]: number } = {}
+		const addToItemFrequency = (id: string) => {
+			if (!itemFrequency[id]) {
+				itemFrequency[id] = 0
+			}
+			itemFrequency[id] += 1
 		}
+		let totalPrice = 0
+		orders.forEach((order) => {
+			totalPrice += order.price
+			order.bagels.forEach((bagel) => {
+				addToItemFrequency(bagel.bagel)
+				bagel.toppings.forEach((topping) => addToItemFrequency(topping))
+			})
+			order.beverages.forEach((beverageId) => addToItemFrequency(beverageId))
+		})
 
-		const item = await Item.findOne().lean();
-		const order = await Order.findOne().lean();
-		const total = 0;
-		for (const [price, Number] of Object.entries(order)) {
-			total = total + Number;
-		}
-		const analysis = [item, order, total]
+		items.forEach((item) => {
+			item.usedInOrders = itemFrequency[item._id] || 0
+		})
 
-		return res.status(200).json({ data: analysis })
+		return res.status(200).json({ data: { items, orders, totalPrice } })
 	}
 )
 
