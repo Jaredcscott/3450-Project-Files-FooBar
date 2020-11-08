7ac5aed39eafb4fd492b0bf584b7e8261e186541
jaredcscott <jaredcscott93@gmail.com>
2020-11-07 22:05:29 -0700

    fixing the theme

--- client/src/themes/provider.js
+++ client/src/themes/provider.js
@@ -1,17 +1,16 @@
+
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import { useSelector } from 'react-redux'
 import { getTheme } from '../redux-store/theme'
 import { AVAILABLE_THEMES } from '../redux-store/theme/constants'
-import lightTheme from '../themes/light'
 import darkTheme from '../themes/dark'
 
 const THEMES = {
-	[AVAILABLE_THEMES.LIGHT]: lightTheme,
 	[AVAILABLE_THEMES.DARK]: darkTheme,
 }
 
 export default function Provider(props: { children: React$Node }) {
-	const theme = THEMES[useSelector(getTheme)] || lightTheme
+	const theme = THEMES[useSelector(getTheme)] || darkTheme
 	return <ThemeProvider theme={theme}>{props.children}</ThemeProvider>
 }
