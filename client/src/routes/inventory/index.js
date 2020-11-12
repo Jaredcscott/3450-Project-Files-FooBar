9842fbd68def9f5f48086e09427c1aaee91bffdd
OmegaGiven <9ncjohnson@gmail.com>
2020-11-12 11:53:37 -0700

    adding the inventory erroring on regiter item

--- client/src/routes/inventory/index.js
+++ client/src/routes/inventory/index.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react'
+import React, { Component, useState, Checkbox } from 'react'
 import styled from 'styled-components'
 import { useSelector, useDispatch } from 'react-redux'
 import { useQuery, useQueryCache } from 'react-query'
@@ -6,32 +6,34 @@ import Button from '../../general/Button'
 import { AVAILABLE_THEMES } from '../../redux-store/theme/constants'
 import { getTheme, setTheme } from '../../redux-store/theme'
 import axios from 'axios'
+// import {default_inventory} from './inventory_data'
 
-// const InventoryItemSchema = new Schema(
-// 	{
-// 		category: {
-// 			type: String,
-// 			enum: INVENTORY_ITEM_CATEGORIES_ENUM,
-// 			required: true,
-// 		},
-// 		name: { type: String, required: true },
-// 		quantity: { type: Number, required: true, default: 0 },
-// 		price: { type: Number, required: true },
-// 		onMenu: { type: Boolean, required: true, default: false },
-// 		targetCount: { type: Number, required: true },
-// 	},
-// 	{ minimize: false }
-// )
+function getSignedInUser() {
+	return axios
+		.get('http://localhost:8100/user')
+		.then((res) => {
+			return res.data.data
+		})
+		.catch(() => null)
+}
+
+const ONE_SECOND = 1000 // ms
 
 
 export default function Inventory() {
+    const loggedin = useQuery('user', getSignedInUser, {
+		cacheTime: ONE_SECOND,
+		refetchOnWindowFocus: false,
+	})
+
     const [name, setName] = useState('')
     const [qty, setQty] = useState('')
     const [price, setPrice] = useState('')
     const [targetCount, setTargetCount] = useState('')
     const [category, setCategory] = useState('')
 
-	const queryCache = useQueryCache()
+    const queryCache = useQueryCache()
+
 
     return (
         <Screen>
@@ -44,14 +46,52 @@ export default function Inventory() {
                         onChange={(event) => setName(event.target.value)}
                     />
                 </label>                <br></br>
+ 
                 <label>
-                    Category:{' '}
-                    <input
-                        type="text"
-                        value={category}
-                        onChange={(event) => setCategory(event.target.value)}
-                    />
+                    BEVERAGE: {' '}
+                    <input 
+                        type="checkbox" 
+                        checked={category} 
+                        value = {INVENTORY_ITEM_CATEGORIES}
+                        onChange={(event) => setCategory(event.target.value)} />
+
                 </label>
+                {INVENTORY_ITEM_CATEGORIES.BEVERAGE}
+                <br></br>
+                <label>
+                    SAMMICHE_TOPPINGS: {' '}
+                    <input 
+                        type="checkbox" 
+                        checked={category} 
+                        value = {INVENTORY_ITEM_CATEGORIES}
+                        onChange={(event) => setCategory(event.target.value)} />
+
+                </label>
+                {INVENTORY_ITEM_CATEGORIES.SAMMICHE_TOPPINGS}
+                <br></br>
+                <label>
+                    SMEAR: {' '}
+                    <input 
+                        type="checkbox" 
+                        checked={category} 
+                        value = {INVENTORY_ITEM_CATEGORIES}
+                        onChange={(event) => setCategory(event.target.value)} />
+
+                </label>
+                {INVENTORY_ITEM_CATEGORIES.SMEAR}
+                <br></br>
+                <label>
+                    BAGEL: {' '}
+                    <input 
+                        type="checkbox" 
+                        checked={category} 
+                        value = {INVENTORY_ITEM_CATEGORIES}
+                        onChange={(event) => setCategory(event.target.value)} />
+
+                </label>
+                {INVENTORY_ITEM_CATEGORIES.BAGEL}
+
+
                 <label>                <br></br>
                     Quantity:{' '}
                     <input
@@ -83,21 +123,26 @@ export default function Inventory() {
                 </Button>
                 <Button
                     color="primary"
-                    onClick={() => addItem(name, category, qty, price, targetCount, queryCache)}>
+                    onClick={() => addDefault(loggedin, queryCache)}>
                     Populate Database
                 </Button>
             </div>
+
+
+
+            
         </Screen>
     )
 	
 }
 
-const INVENTORY_ITEM_CATEGORIES = {
-	BEVERAGE: 'BEVERAGE',
-	SAMMICHE_TOPPINGS: 'SAMMICHE_TOPPINGS',
-	SMEAR: 'SMEAR',
-	BAGEL: 'BAGEL',
-}
+
+const INVENTORY_ITEM_CATEGORIES = [
+	{BEVERAGE: 'BEVERAGE',},
+	{SAMMICHE_TOPPINGS: 'SAMMICHE_TOPPINGS',},
+	{SMEAR: 'SMEAR',},
+	{BAGEL: 'BAGEL',}
+]
 
 function addItem(
     name: string,
@@ -118,6 +163,64 @@ function addItem(
 		})
 }
 
+function addDefault(
+    loggedin: res.data.data,
+    queryCache: any,
+) {
+    var i;
+    for (i = 0; i < default_inventory.length; i++) {
+    var name = default_inventory[i].name
+	var category = default_inventory[i].category
+	var quantity = default_inventory[i].quantity
+    var price = default_inventory[i].price
+    var onMenu = false
+    var targetCount = default_inventory[i].targetCount
+
+    axios
+    .post(`http://localhost:8100/inventory`, {category, name, quantity, price, onMenu, targetCount})
+    .then(() => {
+        console.log('successful added item')
+    })
+    .catch((err) => {
+        console.log('failed to add item')
+        console.error(err)
+    })
+    
+    }
+
+}
+
+
+// getItem needs refactored
+function getItem(
+    loggedin: res.data.data,
+    queryCache: any,
+) {
+    var i;
+    for (i = 0; i < default_inventory.length; i++) {
+    var name = default_inventory[i].name
+	var category = default_inventory[i].category
+	var quantity = default_inventory[i].quantity
+    var price = default_inventory[i].price
+    var onMenu = false
+    var targetCount = default_inventory[i].targetCount
+
+    axios
+    .post(`http://localhost:8100/inventory/${loggedin.data._id}`, {category, name, quantity, price, onMenu, targetCount})
+    .then(() => {
+        console.log('successful added item')
+    })
+    .catch((err) => {
+        console.log('failed to add item')
+        console.error(err)
+    })
+    
+    }
+
+}
+
+
+
 const Screen = styled.div`
 	width: 100%;
 	flex: 1 0 0;
@@ -126,3 +229,31 @@ const Screen = styled.div`
 	align-items: center;
 	background-color: ${({ theme }) => theme.color.base.darker};
 `
+
+
+
+// var default_inventory = [
+//     [
+//     {category: INVENTORY_ITEM_CATEGORIES.BAGEL},
+//     {name: "Plain"},
+//     {quantity: 100},
+//     {price: 200},
+//     {onMenu: true},
+//     {targetCount: 50}
+//     ],
+
+//   ]
+
+
+
+  var default_inventory = [
+    [
+    {category: INVENTORY_ITEM_CATEGORIES.BAGEL},
+    {name: 'tomato'},
+    {quantity: 2},
+    {price: 1},
+    {onMenu: false},
+    {targetCount: 100}
+    ],
+
+  ]
---
+++ client/src/routes/inventory/inventory_data.js
@@ -0,0 +1,169 @@
+var default_inventory = [
+    [
+    {category: INVENTORY_ITEM_CATEGORIES.BAGEL},
+    {name: "Plain"},
+    {qty: 100},
+    {price: 200},
+    {targetCount: 50}
+    ],
+    [
+    {category: INVENTORY_ITEM_CATEGORIES.BAGEL},
+    {name: "Onion"},
+    {qty: 100},
+    {price: 200},
+    {targetCount: 50}
+    ],
+    [
+    {category: INVENTORY_ITEM_CATEGORIES.BAGEL},
+    {name: "Cinnamon raisin"},
+    {qty: 100},
+    {price: 200},
+    {targetCount: 50}
+    ],
+    [
+    {category: INVENTORY_ITEM_CATEGORIES.BAGEL},
+    {name: "Sesame"},
+    {qty: 100},
+    {price: 200},
+    {targetCount: 50}
+    ],
+    [
+    {category: INVENTORY_ITEM_CATEGORIES.BAGEL},
+    {name: "Cheesy"},
+    {qty: 100},
+    {price: 200},
+    {targetCount: 50}
+    ],
+    [
+    {category: INVENTORY_ITEM_CATEGORIES.BAGEL},
+    {name: "Pumpernickel"},
+    {qty: 100},
+    {price: 2},
+      ],
+    [
+    {category: INVENTORY_ITEM_CATEGORIES.SMEAR},
+    {name: "Plain"},
+    {qty: 100},
+    {price: 100},
+    {targetCount: 50}      
+      ],
+    [
+    {category: INVENTORY_ITEM_CATEGORIES.SMEAR},
+    {name: "Honey_nut"},
+    {qty: 100},
+    {price: 100},
+    {targetCount: 50}      
+      ],
+    [
+    {category: INVENTORY_ITEM_CATEGORIES.SMEAR},
+    {name: "Strawberry"},
+    {qty: 100},
+    {price: 100},
+    {targetCount: 50}      
+      ],
+    [
+    {category: INVENTORY_ITEM_CATEGORIES.SMEAR},
+    {name: "French_onion"},
+    {qty: 100},
+    {price: 100},
+    {targetCount: 50}
+    ],
+    [
+    {category: INVENTORY_ITEM_CATEGORIES.SAMMICHE_TOPPINGS},
+    {name: "Bacon"},
+    {qty: 100},
+    {price: 100},
+    {targetCount: 50}
+    ],
+    [
+    {category: INVENTORY_ITEM_CATEGORIES.SAMMICHE_TOPPINGS},
+    {name: "Egg"},
+    {qty: 100},
+    {price: 200},
+    {targetCount: 50}
+    ],
+    [
+    {category: INVENTORY_ITEM_CATEGORIES.SAMMICHE_TOPPINGS},
+    {name: "Cheese"},
+    {qty: 100},
+    {price: 100},
+    {targetCount: 50}
+    ],
+    [
+    {category: INVENTORY_ITEM_CATEGORIES.SAMMICHE_TOPPINGS},
+    {name: "Sausage"},
+    {qty: 100},
+    {price: 200},
+    {targetCount: 50}
+    ],
+    [
+    {category: INVENTORY_ITEM_CATEGORIES.SAMMICHE_TOPPINGS},
+    {name: "Avocado"},
+    {qty: 100},
+    {price: 1000},
+    {targetCount: 50}
+    ],
+    [
+    {category: INVENTORY_ITEM_CATEGORIES.SAMMICHE_TOPPINGS},
+    {name: "Turkey"},
+    {qty: 100},
+    {price: 200},
+    {targetCount: 50}
+    ],
+    [
+    {category: INVENTORY_ITEM_CATEGORIES.SAMMICHE_TOPPINGS},
+    {name: "Ham"},
+    {qty: 100},
+    {price: 200},
+    {targetCount: 50}
+    ],
+    [
+    {category: INVENTORY_ITEM_CATEGORIES.SAMMICHE_TOPPINGS},
+    {name: "Spinach"},
+    {qty: 100},
+    {price: 100},
+    {targetCount: 50}
+    ],
+    [
+    {category: INVENTORY_ITEM_CATEGORIES.SAMMICHE_TOPPINGS},
+    {name: "Tomato"},
+    {qty: 100},
+    {price: 100},
+    {targetCount: 50}
+    ],
+    [
+    {category: INVENTORY_ITEM_CATEGORIES.SAMMICHE_TOPPINGS},
+    {name: "Lox"},
+    {qty: 100},
+    {price: 1000},
+    {targetCount: 50}
+    ],
+    [
+    {category: INVENTORY_ITEM_CATEGORIES.BEVERAGE},
+    {name: "Coffee"},
+    {qty: 100},
+    {price: 200},
+    {targetCount: 50}
+    ],
+    [
+    {category: INVENTORY_ITEM_CATEGORIES.BEVERAGE},
+    {name: "Milk"},
+    {qty: 100},
+    {price: 200},
+    {targetCount: 50}
+    ],
+    [
+    {category: INVENTORY_ITEM_CATEGORIES.BEVERAGE},
+    {name: "OJ"},
+    {qty: 100},
+    {price: 200},
+    {targetCount: 50}
+      ],
+    [
+    {category: INVENTORY_ITEM_CATEGORIES.BEVERAGE},
+    {name: "Water"},
+    {qty: 100},
+    {price: 500},
+    {targetCount: 50}
+    ]
+  ]
