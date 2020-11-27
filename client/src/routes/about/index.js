cba5765739149d5379289072e7a2980ee328636f
jaredcscott <jaredcscott93@gmail.com>
2020-11-27 15:34:28 -0700

    fixing a spelling error

--- client/src/routes/about/index.js
+++ client/src/routes/about/index.js
@@ -70,7 +70,7 @@ export default function About() {
                                         has now blossomed into the bagel kingdom we know as The Dan's Bagel Shop. Dan's was started by the intrepid entrepreneur Mr. Dan.
                                         in 1965 Dan sought to be the worlds greatest bagel supplier.<br></br><br></br>
                                         He bought his first cart for $250, and worked out a deal with the local bakery to supply him with fresh made bagels each morning. Him and his wife, Daniel, would hand make the next day's smears 
-                                        each night in their small studio apartment. When the lines for the cart started to become unmanageable, Dans bought the first brick and mortar
+                                        each night in their small studio apartment. When the lines for the cart started to become unmanageable, Dan bought the first brick and mortar
                                         Dan's Bagel Shop location.<br></br><br></br>
                                         Today there are over 1 locations, and Dan's is moving into the 21st century bu incorporating this high-tech web application. 
                                         We thank you for being a member of the Dan's family and hope you enjoy the worlds greatest bagels!
@@ -162,7 +162,7 @@ export default function About() {
                                         has now blossomed into the bagel kingdom we know as The Dan's Bagel Shop. Dan's was started by the intrepid entrepreneur Mr. Dan.
                                         in 1965 Dan sought to be the worlds greatest bagel supplier.<br></br><br></br>
                                         He bought his first cart for $250, and worked out a deal with the local bakery to supply him with fresh made bagels each morning. Him and his wife, Daniel, would hand make the next day's smears 
-                                        each night in their small studio apartment. When the lines for the cart started to become unmanageable, Dans bought the first brick and mortar
+                                        each night in their small studio apartment. When the lines for the cart started to become unmanageable, Dan bought the first brick and mortar
                                         Dan's Bagel Shop location.<br></br><br></br>
                                         Today there are over 1 locations, and Dan's is moving into the 21st century bu incorporating this high-tech web application. 
                                         We welcome you to join the Dan's family and enjoy the worlds greatest bagels!
